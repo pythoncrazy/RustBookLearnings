e2508e095b2ee17b23fa3fc2de44f627ec806228
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-07-15 00:41:12 -0700

    Chapter 1 Completed!
    This much closer to becoming pro ruster

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-fd8a25d5961bc46b.d
@@ -0,0 +1,5 @@
+C:\Users\viksp\Documents\GitHub\RustBookLearnings\projects\hello_cargo\target\debug\deps\hello_cargo-fd8a25d5961bc46b.rmeta: src\main.rs
+
+C:\Users\viksp\Documents\GitHub\RustBookLearnings\projects\hello_cargo\target\debug\deps\hello_cargo-fd8a25d5961bc46b.d: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\viksp\Documents\GitHub\RustBookLearnings\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\viksp\Documents\GitHub\RustBookLearnings\projects\hello_cargo\src\main.rs
