6de1ce7ff499858684b806a5e4b8478a5eb5fdd6
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-07-15 01:02:38 -0700

    Making the guessing game in both their format
    I also am making a chess themed thing on the side, so I can re-enforce my knowledge or something along those lines.

---
+++ projects/chess_guessing/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    println!("What is the best opening move?");
+    println!("Make it in standard PGN Notation (aka 3. Ke2#)");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the line :(");
+    println!("Imagine guessing: {guess}"); // The Best opening is 1.Ke2!!
+}
---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Guess a Random Number!");
+    println!("The number is between 1 and 100!")
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Put a vaild number pls!");
+    println!("Your guess was: {guess}");
+}
