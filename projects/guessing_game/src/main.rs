21b0f4ab6d12763f28e01f62f39750c1576fa5c8
pythoncrazy <pinakpaliwal@gmail.com>
2023-08-14 06:50:11 +0000

    Sunday, August 13, 2023 at 11:50 PM Pacific Daylight Time

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 fn main() {
     println!("Guess a Random Number!");
     println!("The number is between 1 and 100!");
-
+    let secret_number = rand::thread_rng().gen_range(1..=101);
     let mut guess = String::new();
 
     io::stdin()
