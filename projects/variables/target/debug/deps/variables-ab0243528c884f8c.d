b89b052963df0d9cbc740f1dbe72fcb3f5128fff
pythoncrazy <pinakpaliwal@gmail.com>
2023-10-02 06:57:17 +0000

    Sunday, October 1, 2023 at 11:57 PM Pacific Daylight Time

---
+++ projects/variables/target/debug/deps/variables-ab0243528c884f8c.d
@@ -0,0 +1,5 @@
+/home/ecpinpal/RustBookLearnings/projects/variables/target/debug/deps/variables-ab0243528c884f8c.rmeta: src/main.rs
+
+/home/ecpinpal/RustBookLearnings/projects/variables/target/debug/deps/variables-ab0243528c884f8c.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/deps/variables-f371a0c56e761543.d
@@ -0,0 +1,5 @@
+/home/ecpinpal/RustBookLearnings/projects/variables/target/debug/deps/variables-f371a0c56e761543.rmeta: src/main.rs
+
+/home/ecpinpal/RustBookLearnings/projects/variables/target/debug/deps/variables-f371a0c56e761543.d: src/main.rs
+
+src/main.rs:
