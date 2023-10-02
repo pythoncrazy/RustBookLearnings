4bd13fdb364bf135ce34b9325cb658203c05e66e
pythoncrazy <pinakpaliwal@gmail.com>
2023-10-02 06:58:54 +0000

    Sunday, October 1, 2023 at 11:58 PM Pacific Daylight Time

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -3,4 +3,5 @@ fn main() {
     let mut x: i64 = 3; // The only use is to be printed, and I love type annotation, it's so nice
     println!("x is a variable, and is {x}");
     x = 4;
+    println!("x is now, {x}");
 }
